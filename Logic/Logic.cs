a3e6b21220e30c79651ae5ffdcb2fa39a3cc1d04
Aleksander Gencel <johnnylittleplanet@gmail.com>
2025-04-16 22:58:50 +0200

    Stworzenie logiki

--- Logic/Logic.cs
+++ Logic/Logic.cs
@@ -1,7 +1,58 @@
-﻿namespace Logic
+﻿using Data;
+
+namespace Logic
 {
     public class Logic
     {
+        public IEntity EntityData { get; set; }
 
+        public void Move(int maxWidth, int maxHeight)
+        {
+            EntityData.X += EntityData.MovX;
+            EntityData.Y += EntityData.MovY;
+            if (EntityData.X <= 0 || EntityData.X + EntityData.Radius >= maxWidth)
+            {
+                EntityData.MovX = -EntityData.MovX;
+            }
+            if (EntityData.Y <= 0 || EntityData.Y + EntityData.Radius >= maxHeight)
+            {
+                EntityData.MovY = -EntityData.MovY;
+            }
+        }
+
+        private double CalculateDistance(double dx, double dy)
+        {
+            return Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+        }
+
+        public bool HasCollided(Logic other)
+        {
+            double dx = (EntityData.X + EntityData.Radius / 2) - (other.EntityData.X + other.EntityData.Radius / 2);
+            double dy = (EntityData.Y + EntityData.Radius / 2) - (other.EntityData.Y + other.EntityData.Radius / 2);
+            double distance = CalculateDistance(dx, dy);
+            double minDistance = (EntityData.Radius / 2 + other.EntityData.Radius / 2) * 1.1;
+            return distance <= minDistance;
+        }
+
+        public void ResolveCollision(Logic other)
+        {
+            double dx = (EntityData.X + EntityData.Radius / 2) - (other.EntityData.X + other.EntityData.Radius / 2);
+            double dy = (EntityData.Y + EntityData.Radius / 2) - (other.EntityData.Y + other.EntityData.Radius / 2);
+            double distance = CalculateDistance(dx, dy);
+
+            double minDistance = (EntityData.Radius / 2 + other.EntityData.Radius / 2) * 1.1;
+            if (distance < minDistance)
+            {
+                double overlap = minDistance - distance;
+                double knockX = dx / distance * (overlap / 2);
+                double knockY = dy / distance * (overlap / 2);
+
+                EntityData.X += knockX;
+                EntityData.Y += knockY;
+                other.EntityData.X -= knockX;
+                other.EntityData.Y -= knockY;
+            }
+            (EntityData.MovX, EntityData.MovY) = (other.EntityData.MovX, other.EntityData.MovY);
+        }
     }
 }
